7632d95cd6d442a6b7baf8af7b07281565afb11c
Marc Chung <mchung@gmail.com>
2009-01-07 13:54:50 -0700

    Changed emacs init and irbrc

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,3 +1,33 @@
+(add-to-list 'exec-path "/opt/local/bin")
+(add-to-list 'exec-path "/usr/local/mysql/bin")
+(add-to-list 'exec-path "/opt/local/sbin")
+(add-to-list 'exec-path (getenv "PATH"))
+
 (add-to-list 'load-path "~/.emacs.d/")
+
+;
+; Globals 
+;
+
+; Make Command == Meta
 (setq mac-command-modifier 'meta
       mac-option-modifier 'alt) 
+
+; Use 'y' instead of 'yes'
+(fset 'yes-or-no-p 'y-or-n-p)
+
+; Hide startup message
+(setq inhibit-startup-message t)
+
+
+;
+; Ruby on Rails
+;
+
+; git clone git://github.com/eschulte/rinari.git
+; git-submodule update --init util/jump
+(add-to-list 'load-path "~/.emacs.d/rinari")
+(require 'rinari)
+(global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
+
+
--- .irbrc
+++ .irbrc
@@ -1,7 +1,18 @@
 require "irb/completion"
 require "irb/ext/save-history"
 require "pp"
+#require "rubygems"
+#require "ruby2ruby"
+#require "parse_tree_extensions"
 
 IRB.conf[:SAVE_HISTORY] = 100
 IRB.conf[:HISTORY_FILE] = "#{ENV['HOME']}/.irb-save-history"
+
+# http://ozmm.org/posts/time_in_irb.html
+def time(times = 1)
+  require 'benchmark'
+  ret = nil
+  Benchmark.bm { |x| x.report { times.times { ret = yield } } }
+  ret
+end
 
